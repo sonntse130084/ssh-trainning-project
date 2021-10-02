f6ed2f04779f3356b7d7d7f3910b21c51337e507
SonNT67 <sh2812995@gmail.com>
2021-10-02 10:27:10 +0700

    hello son map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World"/>
+    <HelloWorld msg="Hello Sơn Mập"/>
   </div>
 </template>
 
